b18f348d39980d41924010cb863840f272e6251d
episodeyang <yangge1987@gmail.com>
2013-09-22 11:53:47 -0500

    added more spegetti fixes to Yifan's WTF code. There are simply too many things wrong here.

--- js/spruce.js
+++ js/spruce.js
@@ -5,46 +5,47 @@ angular.module('spruce', []).
       transclude: false,    
       template:
       '<div class="navbar-wrapper">'+
-      '<div class="container">'+
-        '<div class="navbar navbar-inverse navbar-static-top">'+
-          '<div class="container">'+
-            '<div class="navbar-header">'+
-              '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">'+
-                '<span class="icon-bar"></span>'+
-                '<span class="icon-bar"></span>'+
-                '<span class="icon-bar"></span>'+
-              '</button>'+
-              '<a class="navbar-brand" href="index.html">云杉教育</a>'+
+        '<div class="container">'+
+          '<nav class="navbar navbar-inverse navbar-static-top" role="navigation">'+
+            '<div class="container">'+
+              '<div class="navbar-header">'+
+                '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">'+
+                  '<span class="icon-bar"></span>'+
+                  '<span class="icon-bar"></span>'+
+                  '<span class="icon-bar"></span>'+
+                '</button>'+
+                '<a class={{navbrand+focus.home}} href="index.html">云杉教育</a>'+
+              '</div>'+
+              '<div class="navbar-collapse collapse">'+
+                '<ul class="nav navbar-nav">'+
+//                  '<li class={{focus.home}}><a href="index.html">主页</a></li>'+
+                  '<li class={{focus.about}}><a href="about.html">团队介绍</a></li>'+
+                  '<li class={{focus.cases}}><a href="comingsoon.html">案例分析</a></li>'+
+                  '<li class={{focus.contact}}><a href="contact.html">联系我们</a></li>'+
+                  '<li class="dropdown {{focus.software}}">'+
+                    '<a href="comingsoon.html" class="dropdown-toggle" data-toggle="dropdown">软件服务<b class="caret"></b></a>'+
+                    '<ul class="dropdown-menu">'+
+                      '<li><a href="comingsoon.html">管理系统</a></li>'+
+                      '<li><a href="comingsoon.html">智能习题</a></li>'+
+                      '<li class="divider"></li>'+
+                      '<li class="dropdown-header">数据分析</li>'+
+                      '<li><a href="comingsoon.html">数据挖掘</a></li>'+
+                      '<li><a href="comingsoon.html">数据视觉化</a></li>'+
+                    '</ul>'+
+                  '</li>'+
+                '</ul>'+
+              '</div>'+
             '</div>'+
-            '<div class="navbar-collapse collapse">'+
-              '<ul class="nav navbar-nav">'+
-                '<li class={{focus.home}}><a href="index.html">主页</a></li>'+
-                '<li class={{focus.about}}><a href="about.html">团队介绍</a></li>'+
-                '<li class={{focus.cases}}><a href="comingsoon.html">案例分析</a></li>'+
-                '<li class={{focus.contact}}><a href="contact.html">联系我们</a></li>'+
-                '<li class="dropdown {{focus.software}}">'+
-                  '<a href="comingsoon.html" class="dropdown-toggle" data-toggle="dropdown">软件服务<b class="caret"></b></a>'+
-                  '<ul class="dropdown-menu">'+
-                    '<li><a href="comingsoon.html">管理系统</a></li>'+
-                    '<li><a href="comingsoon.html">智能习题</a></li>'+
-                    '<li class="divider"></li>'+
-                    '<li class="dropdown-header">数据分析</li>'+
-                    '<li><a href="comingsoon.html">数据挖掘</a></li>'+
-                    '<li><a href="comingsoon.html">数据视觉化</a></li>'+
-                  '</ul>'+
-                '</li>'+
-              '</ul>'+
-            '</div>'+
-          '</div>'+
+          '</nav>'+
         '</div>'+
-      '</div>'+
-    '</div>',
+      '</div>',
     replace: true,
     link: function (scope, element, attrs) {
       scope.focus = {};
       scope.focus.home = scope.focus.cases = scope.focus.about  = scope.focus.contact = scope.focus.software = '';
       console.log(attrs.infocus);
       scope.focus[attrs.infocus] = 'active';
+      scope.navbrand="navbar-brand ";
     }
     };
   }).
