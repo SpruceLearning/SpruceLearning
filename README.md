a0d915270a5651e3109675b5d68baf07ac1f4827
yifanwu <yifan1030@gmail.com>
2013-09-22 09:04:56 +0800

    new version

--- README.md
+++
@@ -1,4 +0,0 @@
-sprucehome
-==========
-
-spruceHome
--- js/spruce.js
+++ js/spruce.js
@@ -2,15 +2,9 @@ angular.module('spruce', []).
   directive('spruceNav', function() {
     return {
       restrict: 'E',
-      transclude: true,
-      // scope: {
-      //   // infocus: "@",
-      //   // focus: '='
-      // },      
+      transclude: false,    
       template:
       '<div class="navbar-wrapper">'+
-      // '<p>{{focus.about}}</p>'+
-      //  '<p>{{infocus}}</p>'+
       '<div class="container">'+
         '<div class="navbar navbar-inverse navbar-static-top">'+
           '<div class="container">'+
@@ -20,23 +14,23 @@ angular.module('spruce', []).
                 '<span class="icon-bar"></span>'+
                 '<span class="icon-bar"></span>'+
               '</button>'+
-              '<a class="navbar-brand" href="#">云杉教育</a>'+
+              '<a class="navbar-brand" href="index.html">云杉教育</a>'+
             '</div>'+
             '<div class="navbar-collapse collapse">'+
               '<ul class="nav navbar-nav">'+
                 '<li class={{focus.home}}><a href="index.html">主页</a></li>'+
                 '<li class={{focus.about}}><a href="about.html">团队介绍</a></li>'+
-                '<li class={{focus.cases}}><a href="cases.html">案例分析</a></li>'+
+                '<li class={{focus.cases}}><a href="comingsoon.html">案例分析</a></li>'+
                 '<li class={{focus.contact}}><a href="contact.html">联系我们</a></li>'+
-                '<li class="dropdown">'+
-                  '<a href="#" class="dropdown-toggle" data-toggle="dropdown">软件服务<b class="caret"></b></a>'+
+                '<li class="dropdown {{focus.software}}">'+
+                  '<a href="comingsoon.html" class="dropdown-toggle" data-toggle="dropdown">软件服务<b class="caret"></b></a>'+
                   '<ul class="dropdown-menu">'+
-                    '<li><a href="#">管理系统</a></li>'+
-                    '<li><a href="#">智能习题</a></li>'+
+                    '<li><a href="comingsoon.html">管理系统</a></li>'+
+                    '<li><a href="comingsoon.html">智能习题</a></li>'+
                     '<li class="divider"></li>'+
                     '<li class="dropdown-header">数据分析</li>'+
-                    '<li><a href="#">数据挖掘</a></li>'+
-                    '<li><a href="#">数据视觉化</a></li>'+
+                    '<li><a href="comingsoon.html">数据挖掘</a></li>'+
+                    '<li><a href="comingsoon.html">数据视觉化</a></li>'+
                   '</ul>'+
                 '</li>'+
               '</ul>'+
@@ -48,9 +42,30 @@ angular.module('spruce', []).
     replace: true,
     link: function (scope, element, attrs) {
       scope.focus = {};
-      scope.focus.home = scope.focus.cases = scope.focus.about  = scope.focus.contact = '';
+      scope.focus.home = scope.focus.cases = scope.focus.about  = scope.focus.contact = scope.focus.software = '';
       console.log(attrs.infocus);
       scope.focus[attrs.infocus] = 'active';
     }
     };
-  })
+  }).
+  directive('spruceFooter', function() {
+    return {
+      restrict: 'E',
+      transclude: false,     
+      template:
+      '<footer>'+
+        '<p class="pull-right"><a href="#">Back to top</a></p>'+
+        '<p>&copy; 2013 Company, Inc. &middot; <a href="comingsoon.html">Privacy</a> &middot; <a href="comingsoon.html">Terms</a></p>'+
+      '</footer>',
+      replace: true
+    };
+  }).
+  // controller for the form to be filled when contacting
+  controller('ContactCtrl', ['$scope', function($scope) {
+    $scope.message = '';
+    $scope.sendMsg = function () {
+    // TODO for the reader: actually save user to database... 
+    $scope.message = 'Thanks, ' + $scope.user.name + ', we added you!';
+    };
+
+  }])
